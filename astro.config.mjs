56b5c9ed61ceae1e812c7bed2cdb3136620d87f5
carbonomix <152077311+carbonomix@users.noreply.github.com>
2023-12-17 17:42:07 +0530

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 import qwikdev from "@qwikdev/astro";
 import vue from "@astrojs/vue";
+import swup from '@swup/astro';
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [UnoCSS(), vue({}), qwikdev(), solidJs()]
+  integrations: [UnoCSS(),vue({}), qwikdev(), solidJs()]
 });
