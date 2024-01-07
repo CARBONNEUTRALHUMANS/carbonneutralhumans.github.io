cc58b582203616edb7327b808361ede0632dfbce
carbonomix <152077311+carbonomix@users.noreply.github.com>
2024-01-07 22:05:21 +0530

    Add files via upload

--- src/components/t.vue
+++ src/components/t.vue
@@ -6,10 +6,12 @@
     }>()
     const qr = ref<HTMLElement>()
     const a = ref<HTMLElement>()
-
+    let origin = ref("");
     onMounted(() => {
+        origin.value = location.origin;
+
         new Qrcode(qr.value, {
-            text:'http://localhost:4321/mytree?i='+index,
+            text:`${location.origin}/mytree?i=`+index,
             height:150,
             width:150,
             logo: '/btree.png'
@@ -18,7 +20,7 @@
 </script>
 
 <template>
-    <a ref="a" class="my-5 inline-block" :href="`http://localhost:4321/mytree?i=${index}`">
+    <a ref="a" class="my-5 inline-block" :href="`${origin}/mytree?i=${index}`">
 
         <h1>    
             <div ref="qr" class="qr"></div>
