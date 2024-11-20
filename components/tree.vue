e3a3b1a6bebc52270db472d8d8b9e2474ed25c35
carbonomix <152077311+carbonomix@users.noreply.github.com>
2024-11-19 22:28:16 -0800

    Update tree.vue

--- components/tree.vue
+++ components/tree.vue
@@ -1,44 +1,40 @@
 <script setup lang="ts">
 import { onMounted, ref, type Ref } from 'vue';
 import { data, type Tree, type User } from '../components/db';
+import { getAge } from './passbook';
+
     const r = ref<HTMLElement>()
-    const currentUser = ref<User>()
-    const tree = ref<Tree>() as Ref<Tree>
+    const tree = ref<Tree>(data[0].trees[0])
     onMounted(() => {
-        let username = localStorage.getItem("user");
-
-        currentUser.value = data.filter((el) => {
-
-            return el.firstName == username
-        })[0]
         const url = new Proxy(new URLSearchParams(window.location.search), {
           get: (searchParams, prop:string) => searchParams.get(prop),
         }) as any;
-        tree.value = currentUser.value.trees[url.i]
-        console.log(tree.value,currentUser.value)
+        tree.value = data[0].trees[url.i]
+        console.log(tree.value)
     })
 </script>
 
 <template>
-        <div v-if="currentUser" class="w-100vw grid place-items-center grid-cols-2 font-sans overflow-hidden" style="height: calc(100vh - 64px);">
-            <template v-if="tree.height != 24">
+        <div class="w-100vw grid place-items-center grid-cols-2 font-sans overflow-hidden" style="height: calc(100vh - 64px);">
+            <template v-if="!tree.notFound">
               <div class="w-50vw h-full grid pulse  items-center bg-white">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6374.823124732459!2d75.8939488189096!3d22.71359714188747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1700908975707!5m2!1sen!2sin" style="border:0;" class="w-full h-full" loading="lazy"></iframe>
               </div>
               <div class="">
-                <div class="bg-white p-6 rounded-4 shadow-2xl shadow-black">
+                <div class="bg-white mx-4 p-6 rounded-4 shadow-2xl shadow-black">
                   
                   <h1>Type: {{tree.biologicalType}}</h1>
                   <h1>Generic Type: {{tree.genericType}}</h1>
                   <h1>Day Planted: {{tree.dayPlanted.toLocaleDateString()}}</h1>
               <h1>Height: {{tree.height}} Inches</h1>
-                <h1>Owner Name: {{currentUser.firstName}} {{currentUser.lastName}}</h1>
+                <h1>Owner Name: {{data[0].firstName}} {{data[0].lastName}}</h1>
+                <h1>Carbon Absorption: {{ ((getAge(tree.dayPlanted) * 100) + (getAge(tree.dayPlanted) * 5)) }}</h1>
                 </div>
                 
               </div> 
             </template>
             <template v-else>
-              <h1>Tree Not Found</h1>
+              <h1 class="absolute top-1/2 left-1/2 translate--1/2 text-6xl text-white text-center">Tree Not Found</h1>
             </template>
             </div>
           </template>
